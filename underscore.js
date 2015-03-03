c032231ee16474cff0d173cf5e58cd1f91330952
Justin Ridgewell <justin@ridgewell.name>
2015-03-02 19:48:29 -0500

    without _.isArary

--- underscore.js
+++ underscore.js
@@ -128,7 +128,7 @@
   var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
   var isArrayLike = function(collection) {
     var length = collection != null && collection.length;
-    return _.isArray(collection) || (
+    return (
         typeof length == 'number' &&
         length >= 0 &&
         length <= MAX_ARRAY_INDEX &&
